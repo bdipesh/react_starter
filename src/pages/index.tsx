7a72673dfeedb3a190c75f3691810287399c228b
bdipesh <bdipesh08@gmail.com>
2025-11-01 14:00:17 +0545

    show case started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,32 @@
+import { Card } from '../components/ui/Card.tsx';
+import { Button } from '../components/ui/Button.tsx';
+import { Input } from '../components/ui/Input.tsx';
+import { Select } from '../components/ui/Select.tsx';
+
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <>
+      <h1 className="text-3xl my-6">Dashboard</h1>
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-12 md:col-span-6">
+          <Card title="Buttons" subtitle="Buttons variants">
+            <div className="flex gap-2 mt-4">
+              <Button variant="primary">Primary</Button>
+              <Button variant="outline">Outlined</Button>
+              <Button variant="danger">Danger</Button>
+              <Button variant="secondary">Secondary</Button>
+              <Button variant="ghost">Ghost</Button>
+            </div>
+          </Card>
+        </div>
+        {/*<div className="col-span-12 md:col-span-6">*/}
+        {/*  <Card title="Inputs" subtitle="Inputs Variants">*/}
+        {/*    <Input label="Email" />*/}
+        {/*    <Select label="Email" />*/}
+        {/*  </Card>*/}
+        {/*</div>*/}
+      </div>
+    </>
+  );
 };
 export default Dashboard;
